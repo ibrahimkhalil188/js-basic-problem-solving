3b7750acfaf62089ea6f75bce8a5bb64f94978cf
unknown <ibrahimkhalil.mine@gmail.com>
2022-02-04 16:08:13 +0600

    reverse a sentence

---
+++ revers.js
@@ -0,0 +1,9 @@
+const secntence = "I am a studen of P-hero";
+let reverse = "";
+for (const element of secntence) {
+    reverse = element + reverse;
+
+}
+console.log(reverse)
+
+// array have a build in mathod if you need just google it and apply .
